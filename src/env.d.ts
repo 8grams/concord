0842b292e9ddfbc6ffc4d2758251c8f5401cfffe
Audi Nugraha <audinue@gmail.com>
2025-04-22 11:13:41 +0700

    Implement login flow

---
+++ src/env.d.ts
@@ -0,0 +1,15 @@
+type User = {
+  id: number; // 0 -> admin
+  name: string;
+  picture: string;
+};
+
+declare namespace App {
+  interface SessionData {
+    user: User | null;
+  }
+  interface Locals {
+    user: User;
+    isAdmin: boolean;
+  }
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,35 @@
+import "dotenv/config";
 import type { MiddlewareHandler } from "astro";
 import { Db } from "./db";
 
-export const onRequest: MiddlewareHandler = async (_, next) => {
+export const onRequest: MiddlewareHandler = async (Astro, next) => {
+  const user = await Astro.session.get("user");
+  if (!user) {
+    if (!["/login", "/google", "/login-admin"].includes(Astro.url.pathname)) {
+      return Astro.redirect("/login");
+    }
+  } else {
+    if (["/login", "/google", "/login-admin"].includes(Astro.url.pathname)) {
+      return Astro.redirect("/graph");
+    } else {
+      const isAdmin = user.id === 0;
+      if (
+        !isAdmin &&
+        [
+          "/workspaces",
+          "/users",
+          "/new-workspace",
+          "/new-user",
+          "/delete-workspace",
+          "/delete-user",
+        ].includes(Astro.url.pathname)
+      ) {
+        return Astro.redirect("/graph");
+      }
+      Astro.locals.user = user;
+      Astro.locals.isAdmin = isAdmin;
+    }
+  }
   if (!Db.isInitialized) {
     await Db.initialize();
   }
