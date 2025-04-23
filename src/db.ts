1835dd15ad6872ed85a2eedc2ed77f50973f9d7d
Audi Nugraha <audinue@gmail.com>
2025-04-23 10:28:05 +0700

    Update db

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,10 @@ import {
   Column,
   DataSource,
   Entity,
-  PrimaryGeneratedColumn,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  PrimaryGeneratedColumn
 } from "typeorm";
 
 @Entity()
@@ -34,20 +37,64 @@ export class Workspace extends BaseEntity {
 
   @Column("text", { nullable: true })
   log: string;
+
+  @Column("json", { nullable: true })
+  headers: { key: string; value: string }[];
 }
 
 @Entity()
 export class User extends BaseEntity {
+  @PrimaryColumn("text")
+  email: string;
+
+  @Column("text", { nullable: true })
+  name: string;
+
+  @Column("text", { nullable: true })
+  picture: string;
+}
+
+@Entity()
+export class Proposal extends BaseEntity {
+  @PrimaryColumn("int")
+  workspace: number;
+
+  @PrimaryColumn("text")
+  hash: string;
+
+  @Column("text", { nullable: true })
+  status: "On Review" | "Applied" | "Rejected";
+}
+
+@Entity()
+export class Comment extends BaseEntity {
   @PrimaryGeneratedColumn("identity")
   id: number;
 
+  @Column("int")
+  workspace: number;
+
+  @Column("text")
+  hash: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: "user", referencedColumnName: "email" })
+  user: User;
+
   @Column("text", { nullable: true })
-  email: string;
+  message: string;
 }
 
 export const Db = new DataSource({
   type: "better-sqlite3",
   database: "data/data.db",
   synchronize: true,
-  entities: [Workspace, User],
+  entities: [Workspace, User, Proposal, Comment],
 });
+
+export const admin = {
+  email: "admin@admin",
+  name: "Admin",
+  picture:
+    "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp",
+};
