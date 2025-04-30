9dac4a5b288bf077f95dea1b9fc49628393f14f8
Taufik Nurhidayat <fiik346@gmail.com>
2025-04-30 13:47:10 +0700

    feat: add output directory with list and downloadable api

--- src/db.ts
+++ src/db.ts
@@ -7,7 +7,7 @@ import {
   JoinColumn,
   ManyToOne,
   PrimaryGeneratedColumn,
-  UpdateDateColumn
+  UpdateDateColumn,
 } from "typeorm";
 
 @Entity()
@@ -26,12 +26,15 @@ export class Workspace extends BaseEntity {
 
   @Column("text", { nullable: true })
   currentBranch: string;
-  
+
   @Column("text", { nullable: true })
   mainDirectory: string;
 
   @Column("text", { nullable: true })
   privateKey: string;
+
+  @Column("text", { nullable: true })
+  outputDirectory: string;
 
   @Column("text", { nullable: true })
   status: string;
@@ -103,7 +106,6 @@ export class Proposal extends BaseEntity {
   @JoinColumn({ name: "rejector", referencedColumnName: "id" })
   rejector: User;
 }
-
 
 @Entity()
 export class Comment extends BaseEntity {
